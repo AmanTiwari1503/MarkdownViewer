33c50ec9d14f9e2ca2d7261e79a49f7a9b85646b
AmanTiwari1503 <tiwariaman2000@gmail.com>
2018-12-26 11:19:22 +0530

    Correction in routes

--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -3,6 +3,6 @@ var mainController = require('../controllers/mainController');
 var router = express.Router();
 //console.log('here');
 router.route('/').get(mainController.home);
-router.route('/(:id)').get(mainController.home);
+router.route('/*').get(mainController.home);
 
 module.exports = router;
