a46fbaea578f8a5f2aed977926b7a46412f20864
AmanTiwari1503 <tiwariaman2000@gmail.com>
2018-12-26 11:48:20 +0530

    Corrected controllers

--- backend/controllers/mainController.js
+++ backend/controllers/mainController.js
@@ -2,10 +2,14 @@ const request = require('request');
 
 module.exports = {
 	home:home
+	newhome:newhome
 }
 
 //request handling
 function home(req,res){
+	res.render('pad.ejs',{});
+}
+function newhome(req,res){
 	res.render('pad.ejs',{});
 }
 //console.log('here1');
